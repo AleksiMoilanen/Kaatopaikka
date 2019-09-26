d280d548946f9b9a6958daa08e07ed79becfde6f
Aleksi Moilanen <moil.aleksi@gmail.com>
2019-09-26 19:22:25 +0300

    Shell script files updated

--- Docker - RasPi CrossCompile/Build/build.sh
+++ Docker - RasPi CrossCompile/Build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-/root/raspi/qt5/bin/qmake $1.pro -spec devices/linux-rasp-pi-g++ CONFIG+=debug CONFIG+=qml_debug && /usr/bin/make qmake_all
-
+cd $1
+/root/raspi/qt5/bin/qmake *.pro -spec devices/linux-rasp-pi-g++ CONFIG+=debug CONFIG+=qml_debug && /usr/bin/make qmake_all
 make -j3
 
 sshpass -p $3 scp -P $5 /root/build/$1 $2@$4:/home/pi/build
--- Docker - RasPi CrossCompile/Sync/sync.sh
+++ Docker - RasPi CrossCompile/Sync/sync.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-rsync --rsh="sshpass -p $2 ssh -p $4 -o StrictHostKeyChecking=no" -avz /root/raspi/qt5pi $1@$3:/usr/local
+#sysroot synkronointi Raspilta tietokoneelle
+sshpass -p $2 ssh $1@$3 "ssh sudo apt-get build-dep qt4-x11 -y ; sudo apt-get build-dep libqt5gui5 -y ; sudo apt-get install libudev-dev libinput-dev libts-dev libxcb-xinerama0-dev libxcb-xinerama0 -y ; sudo mkdir /usr/local/qt5pi ; sudo chown pi:pi /usr/local/qt5pi"
+#rsync --rsh="sshpass -p $2 ssh -p $4 -o StrictHostKeyChecking=no" -avz /root/raspi/qt5pi $1@$3:/usr/local
